efeb462de0749c2757431a1c9a65c01ff77c34d5
Jean do Nascimento <jeandonascmento@gmail.com>
2023-06-29 09:45:39 -0300

    initial commit

---
+++ build/precache-manifest.63f687e04545bc301c63c41c8108c703.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce49047a166a4ce91dcab1de2991746",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7eccc04c21f2e251c1b9",
+    "url": "/static/css/main.a2c26a27.chunk.css"
+  },
+  {
+    "revision": "0a7e804c062478eef555",
+    "url": "/static/js/2.416d7a3c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.416d7a3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eccc04c21f2e251c1b9",
+    "url": "/static/js/main.54b6f2ed.chunk.js"
+  },
+  {
+    "revision": "0e26684ae559a6f492dc",
+    "url": "/static/js/runtime-main.038948d4.js"
+  }
+]);
